15bc107b838b642e63e82a5a8f68dce27bc7b713
Mikhail <mikhailhorychau@yandex.by>
2020-06-03 19:13:28 +0300

    Bookmark Form functional added.

--- src/components/BookmarkForm/index.tsx
+++ src/components/BookmarkForm/index.tsx
@@ -1,35 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './index.scss';
 import AutocompleteInput from '../AutocompleteInput';
 import Input from '../Input';
 import Chip from '../Chip';
 
-let test_data = [
-  'c',
-  'c++',
-  'c#',
-  'java',
-  'javascript'
-]
-
-
 interface IBookmarkFormProps {
   
 }
 const BookmarkForm: React.FC<IBookmarkFormProps> = () => {
 
+  const [tags, setTags] = React.useState<string[]>([]);
   const [bookmarkTags, setBookmarkTags] = React.useState<string[]>([]);
+  const [urlInput, setUrlInput] = React.useState('');
+  const [titleInput, setTitleInput] = React.useState('');
+
+  useEffect(() => {
+    const localTags = JSON.parse(localStorage.getItem('tags') || "[]");
+    setTags(localTags);
+  }, [])
 
   const addBookmarkTag = (tag: string) => {
     if(!bookmarkTags.includes(tag)) {
       const newBookmarkTags = [...bookmarkTags, tag].sort();
       setBookmarkTags(newBookmarkTags);
     }
-    if(!test_data.includes(tag)) {
-      test_data.push(tag);
-      test_data.sort();
+    if(!tags.includes(tag)) {
+      const newTags = [...tags, tag].sort();
+      setTags(newTags);
+      localStorage.setItem('tags', JSON.stringify(newTags));
     };
+  }
+
+  const handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    const target = event.target as Element;
+    if(urlInput && titleInput) {
+      const localBookmarks = JSON.parse(localStorage.getItem('bookmarks') || "[]");
+      const newBookmark = {
+        url: urlInput,
+        title: titleInput,
+        tags: bookmarkTags
+      }
+      localStorage.setItem('bookmarks', JSON.stringify([...localBookmarks, newBookmark]));
+      setUrlInput('');
+      setTitleInput('');
+      setBookmarkTags([]);
+    }
+  }
+
+  const handleUrlInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrlInput(event.target.value)
+  }
+
+  const handleTitleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleInput(event.target.value)
   }
 
   const removeBookmarkTag = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -44,14 +69,31 @@ const BookmarkForm: React.FC<IBookmarkFormProps> = () => {
         Add new bookmark
       </div>
       <div className="bookmark-form__input-wrapper">
-        <Input type='text' placeholder='Bookmark URL'/>
+        <Input 
+          type='text' 
+          placeholder='Bookmark URL'
+          name='url'
+          value={urlInput}
+          onChange={handleUrlInputChange}
+          autoComplete='off'
+        />
       </div>
       <div className="bookmark-form__input-wrapper">
-        <Input type='text' placeholder='Bookmark Title'/>
+        <Input 
+          type='text' 
+          placeholder='Bookmark Title'
+          name='title'
+          value={titleInput}
+          onChange={handleTitleInputChange}
+          autoComplete='off'
+        />
       </div>
       <div className="bookmark-form__input-wrapper">
-        <AutocompleteInput data={test_data} addDataHandler={addBookmarkTag} placeholder='tag'/>
+        <AutocompleteInput data={tags} addDataHandler={addBookmarkTag} placeholder='tag'/>
       </div>
+      <button className="bookmark-form__button" onClick={handleSubmit}>
+        Save Bookmark
+      </button>
       <div className='chip-container'>
         <div className="chip-container__title">Tags:</div>
         <div className="chip-container__items">
